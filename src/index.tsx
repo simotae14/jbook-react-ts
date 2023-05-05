5edb211aff0189ea3e2abcb3be7b0bd9b048f75a
Simone Taeggi <startae14@gmail.com>
2023-05-06 00:00:16 +0200

    define a form element with controlled textarea

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 
 const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    console.log(input);
+  }
   return <div>
-    <h1>Hi there!</h1>
+    <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+    <div>
+      <button onClick={onClick}>Submit</button>
+    </div>
+    <pre>{code}</pre>
   </div>
 };
 
