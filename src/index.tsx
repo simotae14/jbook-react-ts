ddddee8d63c50dd0cbc815714c56e0fc05c890fd
Simone Taeggi <startae14@gmail.com>
2023-05-06 10:45:18 +0200

    define a ref to a variable with TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { createRoot } from "react-dom/client";
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   // config the esbuild service
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm'
     });
-    console.log(service);
+    
   };
 
   // call the esbuild service at first rendering
@@ -20,8 +21,17 @@ const App = () => {
     startService();
   }, []);
 
-  const onClick = () => {
-    console.log(input);
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx', // code to transpile
+      target: 'es2015'
+    });
+
+    setCode(result.code);
   }
   return <div>
     <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
