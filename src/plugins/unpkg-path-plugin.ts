899c8035924e57472bedf87047613869aabe9f69
Simone Taeggi <startae14@gmail.com>
2023-05-06 15:07:22 +0200

    connect the text in the textarea with the unpkg plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,29 +1,33 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
-import localforage from 'localforage';
+import localForage from 'localforage';
 
-const fileCache = localforage.createInstance({
-  name: 'filecache'
+const fileCache = localForage.createInstance({
+  name: 'filecache',
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return { 
+          path: new URL(
+            args.path, 
+            'https://unpkg.com' + args.resolveDir + '/'
+          ).href, // to generate the absolute path from a relative 
+          namespace: 'a' 
+        };
+      });
+
+      // Handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return { 
-            path: new URL(
-              args.path, 
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href, // to generate the absolute path from a relative 
-            namespace: 'a' 
-          };
-        }
         return { 
           path: `https://unpkg.com/${args.path}`, 
           namespace: 'a' 
@@ -36,10 +40,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React, { useState } from 'react';  
-              console.log(React, useState);
-            `,
+            contents: inputCode,
           };
         }
 
