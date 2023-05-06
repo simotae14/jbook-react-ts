819e7699d344f3dd9e16c9a3832b03fb968a838a
Simone Taeggi <startae14@gmail.com>
2023-05-06 10:31:22 +0200

    initialize the EsBuild Service

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 
 const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+
+  // config the esbuild service
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+    console.log(service);
+  };
+
+  // call the esbuild service at first rendering
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onClick = () => {
     console.log(input);
