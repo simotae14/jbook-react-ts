af30691a192f89f076afc0f6876208ca02337359
Simone Taeggi <startae14@gmail.com>
2023-05-06 16:13:27 +0200

    Loading CSS Files from textarea

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -22,18 +22,33 @@ export const fetchPlugin = (inputCode: string) => {
 
         // Check to see if we have already fetched this file
         // and if it is in the cache
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-        // if it is, return it immediately
-        if (cachedResult) {
-          return cachedResult;
-        }
+        // // if it is, return it immediately
+        // if (cachedResult) {
+        //   return cachedResult;
+        // }
 
         const { data, request } = await axios.get(args.path);
+
+        // define the loader to use
+        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
         
+        // escape new line character, " and '
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents = fileType === 'css' ?
+          `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+          ` : data;
+
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
-          contents: data,
+          contents,
           resolveDir: new URL('./', request.responseURL).pathname
         };
 
