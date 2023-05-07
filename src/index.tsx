4de669530f76dea13057615122f52442b697285f
Simone Taeggi <startae14@gmail.com>
2023-05-07 10:25:38 +0200

    show out code inside a generated html container

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -41,17 +42,29 @@ const App = () => {
         global: 'window'
       }
     });
-
-    //console.log(result);
-
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   }
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data);
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
   return <div>
     <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
     <div>
       <button onClick={onClick}>Submit</button>
     </div>
     <pre>{code}</pre>
+    <iframe ref={iframe} title='test' srcDoc={html} sandbox="allow-scripts" />
   </div>
 };
 
