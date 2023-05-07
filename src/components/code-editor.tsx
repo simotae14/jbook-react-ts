b4dee907419fa4398a888a1114359edfce186660
Simone Taeggi <startae14@gmail.com>
2023-05-07 12:12:53 +0200

    define the onChange props with Type

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,27 +2,36 @@ import MonacoEditor from "@monaco-editor/react";
 
 interface CodeEditorProps {
   initialValue: string;
+  onChange(value: string): void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
-  return <MonacoEditor
-    value={initialValue} // is the initial value
-    theme="dark" 
-    language="javascript" 
-    height="500px"
-    options={{
-      wordWrap: 'on',
-      minimap: {
-        enabled: false
-      },
-      showUnused: false,
-      folding: false,
-      lineNumbersMinChars: 3,
-      fontSize: 16,
-      scrollBeyondLastLine: false,
-      automaticLayout: true,
-    }} 
-  />;
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+  }
+  return (
+    <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue} // is the initial value
+      theme="dark" 
+      language="javascript" 
+      height="500px"
+      options={{
+        wordWrap: 'on',
+        minimap: {
+          enabled: false
+        },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }} 
+    />
+  );
 };
 
 export default CodeEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -69,7 +69,10 @@ const App = () => {
     </html>
   `;
   return <div>
-    <CodeEditor initialValue="const a = 1;" />
+    <CodeEditor 
+      initialValue="const a = 1;"
+      onChange={(value) => setInput(value)}
+    />
     <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
     <div>
       <button onClick={onClick}>Submit</button>
