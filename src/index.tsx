663acf1345a6bca64e871702dbe89ba9ad88f1c1
Simone Taeggi <startae14@gmail.com>
2023-05-07 16:25:12 +0200

    Add a CSS library, bulmaswatch

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from "react";
 import { createRoot } from "react-dom/client";
