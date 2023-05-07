e846b1f34827e78f7a0ec949e3bb5fa1afe53608
Simone Taeggi <startae14@gmail.com>
2023-05-07 11:07:51 +0200

    Display the Monaco Editor in the project

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 
+import CodeEditor from './components/code-editor';
+
 const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
@@ -67,6 +69,7 @@ const App = () => {
     </html>
   `;
   return <div>
+    <CodeEditor />
     <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
     <div>
       <button onClick={onClick}>Submit</button>
